b8a855f6ebad66018ac4008df9b3b0293289b803
pgd <rudeh1253@gmail.com>
2024-04-06 14:21:20 +0900

    feature: 404 Not Found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import "./App.css";
 import NavHeader from "./component/fragment/NavHeader";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./component/page/Home";
+import NotFound from "./component/page/NotFound";
 
 function App() {
-
     return (
         <>
-            <NavHeader/>
+            <NavHeader />
             <Routes>
-                <Route path="/" element={<Home/>}/>
+                <Route path="/" element={<Home />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </>
     );
---
+++ src/component/page/NotFound.tsx
@@ -0,0 +1,11 @@
+import {Container} from "react-bootstrap";
+
+function NotFound() {
+    return (
+        <Container className="justify-content-md-center">
+            <h1>404 Not Found</h1>
+        </Container>
+    );
+}
+
+export default NotFound;
