ccf639ec8d72389afa4672f765252069d16c2487
pgd <rudeh1253@gmail.com>
2024-04-06 15:09:57 +0900

    chore: add react-cookie as dependency

--- src/component/fragment/NavHeader.tsx
+++ src/component/fragment/NavHeader.tsx
@@ -18,7 +18,7 @@ function NavHeader() {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#">Model 2 MVC Shop</Navbar.Brand>
+                <Navbar.Brand href="/">Model 2 MVC Shop</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
