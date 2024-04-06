da4d41af7cc0f7bbec82aa298bdd08f216b85480
pgd <rudeh1253@gmail.com>
2024-04-06 11:26:01 +0900

    feat(Home): Home page component
    For now, it is just an empty page.
    I'm going to add feature such as products the user visited recently,
    recommended products, and so on.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import NavHeader from "./component/fragment/NavHeader";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./component/page/Home";
 
 function App() {
 
     return (
         <>
             <NavHeader/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+            </Routes>
         </>
     );
 }
---
+++ src/component/page/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+    // TODO: add some features to display at the home of the website
+
+    return (
+        <></>
+    );
+}
+
+export default Home;
