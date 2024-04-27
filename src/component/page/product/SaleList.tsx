acca262423a6e0f1463de018c5501d514bd8caec
pgd <rudeh1253@gmail.com>
2024-04-28 08:54:09 +0900

    feat: seller page

--- src/component/page/product/SaleList.tsx
+++ src/component/page/product/SaleList.tsx
@@ -1,32 +1,63 @@
 import {useParams, useSearchParams} from "react-router-dom";
 import {useAppSelector} from "../../../store/hook";
-import {useEffect} from "react";
-import {Container} from "react-bootstrap";
+import {useEffect, useState} from "react";
+import {Col, Container, Row} from "react-bootstrap";
 import ProductList from "./ProductList";
+import httpRequest from "../../../network/httpRequest";
+import {SellerInfo} from "../../../network/apispec/seller/sellerInfoSpec";
 
 export default function SaleList() {
     const {seller} = useParams();
     const [queryParameters, setQueryParameters] = useSearchParams();
 
+    const [sellerInfo, setSellerInfo] = useState<SellerInfo>();
+
     const apiUrl = useAppSelector((state) => state.metadata.apiUrl);
 
     useEffect(() => {
         if (seller) {
-            console.log(seller);
             queryParameters.set("seller", seller);
             setQueryParameters(queryParameters);
         }
     }, []);
 
     useEffect(() => {
-        if (apiUrl) {
-
+        if (apiUrl && queryParameters.has("seller")) {
+            httpRequest({
+                url: `/api/seller/${queryParameters.get("seller")}`,
+                baseUrl: apiUrl,
+                callback: (response) => {
+                    const data = response.data as SellerInfo;
+                    setSellerInfo(data);
+                }
+            });
         }
-    }, [apiUrl]);
+    }, [apiUrl, queryParameters]);
 
     return (
         <Container>
-            <h2>{seller}</h2>
+            <h2>판매자 정보</h2>
+            <Row>
+                <Col md={4}>
+                    <img
+                        src={
+                            apiUrl && sellerInfo?.profileImageFile
+                                ? `${apiUrl}/images/uploadFiles/${sellerInfo.profileImageFile}`
+                                : ""
+                        }
+                        alt="Profile"
+                        style={{width: "80%"}}
+                    />
+                </Col>
+                <Col md={8}>
+                    <h3>{sellerInfo?.nameOfUser}</h3>
+                    <p>아이디: {sellerInfo?.sellerId}</p>
+                    <p>이메일: {sellerInfo?.email}</p>
+                    <p>연락처: {sellerInfo?.phone}</p>
+                </Col>
+                <h3>소개</h3>
+                <p>{sellerInfo?.profile}</p>
+            </Row>
             <ProductList />
         </Container>
     );
---
+++ src/network/apispec/seller/sellerInfoSpec.ts
@@ -0,0 +1,11 @@
+export type SellerInfo = {
+    sellerId: string;
+    nameOfUser: string;
+    ssn: string;
+    phone: string;
+    addr: string;
+    email: string;
+    regDate: string;
+    profileImageFile: string;
+    profile: string;
+};
