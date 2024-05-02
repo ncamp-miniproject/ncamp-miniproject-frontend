48cb8a357f40dce6b5bd76346105b4adef02f30b
pgd <rudeh1253@gmail.com>
2024-05-03 00:20:21 +0900

    refactor: change api url

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,3 +1,3 @@
 export const ACCESS_TOKEN = "access_token";
 export const REFRESH_TOKEN = "refresh_token";
-export const apiServerUrl = "http://localhost:8089";
+export const apiServerUrl = "https://api.novemberalpha.shop";
